0e415fbce17e6a23c486b95c2c81655d9a01a2df
Thomas Sankara <thomassankara74@gmail.com>
2022-01-11 10:41:47 +0300

    data

---
+++ real-estate-template/data.js
@@ -0,0 +1,90 @@
+// install uuid from npm using
+// ` npm install uuid `
+import { v4 as uuidv4 } from "uuid"
+
+export const homeCards = [
+  {
+    id: uuidv4(),
+    title: "Permanent home",
+    image: "./images/permanent-home.svg",
+    desc: "Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration.",
+    button: "Learn More",
+    url: "/",
+  },
+  {
+    id: uuidv4(),
+    title: "Vacation home",
+    image: "./images/vacation-home.svg",
+    desc: "Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration.",
+    button: "Learn More",
+    url: "/",
+  },
+  {
+    id: uuidv4(),
+    title: "Guaranteed",
+    image: "./images/bank.svg",
+    desc: "Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration.",
+    button: "Learn More",
+    url: "/",
+  },
+]
+
+// You can change the path of the images relative to your folder / workspace
+export const homes = [
+  {
+    id: uuidv4(),
+    image: "./images/home-1.jpg",
+    title: "Bahamas",
+    desc: "Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration. Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration.",
+    url: "/",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/home-2.jpg",
+    title: "Aruba",
+    desc: "Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration. Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration.",
+    url: "/",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/home-3.jpg",
+    title: "Mombasa",
+    desc: "Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration. Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration.",
+    url: "/",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/home-4.jpg",
+    title: "Jamaica",
+    desc: "Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration. Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration.",
+    url: "/",
+  },
+]
+
+// You can change the path of the images relative to your folder / workspace
+export const galleryImages = [
+  {
+    id: uuidv4(),
+    image: "./images/gallery-1.jpg",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/gallery-2.jpg",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/gallery-3.jpg",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/gallery-4.jpg",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/gallery-5.jpg",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/gallery-6.jpg",
+  },
+]
